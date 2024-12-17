30654c30e492370b8c6117af4714de55614d29f5
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-12-17 20:58:36 +0300

    update

---
+++ 2D_Lists_and_Dictionaries.py
@@ -0,0 +1,15 @@
+
+
+#two-dimensional list
+'''
+grades = [[45,37,54],[62,58,59],[49,47,60],[78,83,62]]
+
+print(grades)
+'''
+#alternative dictionary
+'''
+grades = [{"Ma":45, "En":37, "Fr":54}, {"Ma":62, "En":58, "Fr":59},{"Ma":49, "En":47, "Fr":60}]
+print(grades[0]["En"])
+'''
+grades = [{"Ma":45, "En":37, "Fr":54}, {"Ma":62, "En":58, "Fr":59},{"Ma":49, "En":47, "Fr":60}]
+print(grades[0]["En"])
