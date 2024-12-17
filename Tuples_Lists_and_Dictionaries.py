27ace715330c65f5572d1177247a19938232f2fe
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-12-17 19:55:32 +0300

    update

---
+++ Tuples_Lists_and_Dictionaries.py
@@ -0,0 +1,14 @@
+#NOTES
+# 
+# Once a tuple is defined you cannot change what is stored in it.
+
+#The contents of a list can be changed while the program is 
+# running and lists are one of the most common ways to store a
+# collection of data under one variable name in Python.
+
+
+#The contents of a dictionary can also be changed while the program is running.
+
+#EXAMPLE CODE
+
+fruit_tuple = ("apple", "banana", "strawberry", "orange")
