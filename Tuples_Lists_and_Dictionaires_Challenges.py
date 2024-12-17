4a65859b0353639b6cb8097ad44bb775a9f86928
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-12-17 20:45:10 +0300

    update list

---
+++ Tuples_Lists_and_Dictionaires_Challenges.py
@@ -0,0 +1,10 @@
+#069
+
+countries = ('Armenia', 'Jakarta', 'Australia', 'Philippines',  'Brazil')
+
+print(countries)
+
+print()
+
+country = input("Enter one of the listed cuntries")
+print(country, "has index number", countries.index(country))
