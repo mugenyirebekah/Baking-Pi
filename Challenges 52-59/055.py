f406f6a296d23fc25b10e51746b053579c931d0f
Rebekah Mugenyi <rebekahmish16@gmail.com>
2025-03-25 14:13:37 +0300

    update

---
+++ Challenges 52-59/055.py
@@ -0,0 +1,14 @@
+import random
+
+cnum = random.randint(1,5)
+
+hnum = int(input("Enter a number: "))
+
+if hnum == cnum:
+    print("Well done")
+
+else:
+    if hnum < cnum:
+        
+
+print(num)
