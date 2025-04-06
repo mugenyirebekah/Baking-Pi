7c680b6310656593a7412d7c89e27888762d02d9
Rebekah Mugenyi <rebekahmish16@gmail.com>
2025-04-06 15:42:45 +0300

    practice

---
+++ Practice/6-04-25.py
@@ -0,0 +1,65 @@
+# #002
+
+# firstname = input("Please enter your first name: ")
+# surname = input("Last name: ")
+
+
+# print("Hello", firstname, surname)
+
+
+
+# #013
+
+# num = int(input("Enter a number that is under 20: "))
+
+# if num >= 20:
+#     print("Too high")
+
+# else:
+#     print("Thank you")
+
+# #021
+
+# fname = input("Enter your first name: ")
+# lname = input("Enter your last name: ")
+
+# fullname = fname + " " + lname
+# print(fullname)
+
+# print(len(fullname))
+
+
+# #028
+
+# num = float(input("Enter a number with lots of decimal places: "))
+
+
+# fnum = num*2
+
+# print(round(fnum, 2))
+
+
+# #036
+
+# name = input("Enter a name: ")
+# num = int(input("Enter a number: "))
+
+# for i in range(num):
+#     print(name)
+
+# #046
+
+# num = 0
+
+# while num <=5:
+#     num = int(input("Enter a number: "))
+
+# print("The last number you entered was a", num)
+
+# #053
+
+# import random
+
+# fruit = random.choice(['apple', 'banana', 'grape'])
+
+# print(fruit)
