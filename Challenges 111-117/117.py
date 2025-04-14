6402c039fb3d44d7bcb985b52c67dfe0dd5d328b
Rebekah Mugenyi <rebekahmish16@gmail.com>
2025-04-14 20:39:41 +0300

    one must try

--- Challenges 111-117/117.py
+++ Challenges 111-117/117.py
@@ -1,2 +1,23 @@
+import csv
+import random
+score = 0
+name = input("What's your name: ")
 
-name = input("Enter name: ")
+q1_num1 = random.randint(10,50)
+q1_num2 = random.randint(10,50)
+
+question1 = str(q1_num1)+ " + " + str(q1_num2) + " = "
+ans1 = int(input(question1))
+
+realans1 = q1_num1+q1_num2
+
+if ans1 == realans1:
+    score = score + 1
+
+q2_num1 = random.randint(10,50)
+q2_num2 = random.randint(10,50)
+
+question2 = str(q2_num1) + "+" + str(q2_num2) + "="
+ans2 = int(input(question2))
+realans2 = q2_num1+q2_num2
+if ans2 ==
