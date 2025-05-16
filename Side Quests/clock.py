cda7356396b096c42111ccf654ee225f32361192
Rebekah Mugenyi <rebekahmish16@gmail.com>
2025-05-16 23:19:34 +0300

    digital clock

---
+++ Side Quests/clock.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from time import strftime
+
+
+root=tk.Tk()
+root.title("Digital Clock")
+
+clock_label = tk.Label(root, font=("Helvetica",48),bg="black",fg="cyan")
+
+clock_label.pack(anchor="center", fill="both", expand=True)
+
+#Function to update the time
+
+
+def update_time():
+    current_time = strftime("%H:%M:%S")
+    clock_label.config(text=current_time)
+    clock_label.after(1000, update_time)
+
+update_time()
+root.mainloop()
